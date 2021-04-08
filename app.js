ac95fe3dc77bcb0af14cd8b708b9e1a5d5be1dce
Charliemur2 <charliemurciamunoz@gmail.com>
2021-04-08 00:14:07 -0500

    finish addition result functionality, Styles

--- app.js
+++ app.js
@@ -1,51 +1,80 @@
 const tableDiv = document.querySelector('.js-table');
 const submitBtn = document.querySelector('.js-submit');
 const validateDiv = document.querySelector('.js-validation');
+
 submitBtn.addEventListener('click', function() {
-  const tbl = document.createElement('table');
-  const tblBody = document.createElement('tbody');
   const xVal = document.querySelector('.js-x').value;
   const yVal = document.querySelector('.js-y').value;
+  const tbl = document.createElement('table');
+  const tblBody = document.createElement('tbody');
 
   for (let i = 0; i < xVal; i++) {
     const row = document.createElement('tr');
     for (let j = 0; j < yVal; j++) {
       const cell = document.createElement('td');
       cell.classList.add('js-value');
-      const plusText = document.createTextNode('+');
       cell.classList.add('table-cell');
       const cellInput = document.createElement('input');
       cellInput.classList.add(`row${i}-column${j}`);
       cell.appendChild(cellInput);
       row.appendChild(cell);
-      row.appendChild(plusText);
     }
-    const validateBtn = document.createElement('button');
-    const valBtnText = document.createTextNode('validate');
-    validateBtn.appendChild(valBtnText);
-    row.appendChild(validateBtn);
     tblBody.appendChild(row);
-    validateBtn.addEventListener('click', function() {
-      let sum = 0;
-      for (let j = 0; j < yVal; j++) {
-        let value = document.querySelector(`.row${i}-column${j}`).value;
-        let valueNum = Number(value);
-        if (Number.isInteger(valueNum) !== true) {
-          const validateResult = document.createTextNode('Insert only integers please');
-          validateDiv.appendChild(validateResult);
-          return;
-        }
-        sum += valueNum;
-      }
-      if (sum === 100) {
-        const validateResult = document.createTextNode('The addition is equal to 100');
-        validateDiv.appendChild(validateResult);
-      } else {
-        const validateResult = document.createTextNode('The addition is NOT equal to 100');
-        validateDiv.appendChild(validateResult);
-      }
-    });
   }
   tbl.appendChild(tblBody);
   tableDiv.appendChild(tbl);
+});
+const validateBtn = document.querySelector('.js-validate-btn');
+validateBtn.addEventListener('click', function() {
+  const xVal = document.querySelector('.js-x').value;
+  const yVal = document.querySelector('.js-y').value;
+  for (let i = 0; i < xVal; i++) {
+    let sum = 0;
+    for (let j = 0; j < yVal; j++) {
+      let value = document.querySelector(`.row${i}-column${j}`).value;
+      let valueNum = Number(value);
+      if (Number.isInteger(valueNum) !== true) {
+        const validateResult = document.createTextNode('Insert only integers please');
+        validateDiv.appendChild(validateResult);
+        return;
+      }
+      sum += valueNum;
+    }
+    console.log(sum);
+    if (sum !== 100) {
+      const validateResult = document.createTextNode(`Sorry the addition in row number ${i} is not iqual to 100`);
+      validateDiv.appendChild(validateResult);
+      return;
+    }
+  }
+  for (let i = 0; i < yVal; i++) {
+    let sumColumn = 0
+    for (let j = 0; j < xVal; j++) {
+      let valueColumn = document.querySelector(`.row${j}-column${i}`).value;
+      let valueNumColumn = Number(valueColumn);
+      if (Number.isInteger(valueNumColumn) !== true) {
+        const validateResult = document.createTextNode('Insert only integers please');
+        validateDiv.appendChild(validateResult);
+        return j;
+      }
+      sumColumn += valueNumColumn;
+    }
+    console.log(sumColumn);
+    if (sumColumn !== 100) {
+      const validateResult = document.createTextNode(`Sorry the addition in column number is not iqual to 100`);
+      validateDiv.appendChild(validateResult);
+      return;
+    }
+    /* if (sum === 100) {
+      const validateResult = document.createTextNode('The addition is equal to 100');
+      validateDiv.appendChild(validateResult);
+      validateDiv.appendChild(document.createElement('br'));
+    } else {
+      const validateResult = document.createTextNode('The addition is NOT equal to 100');
+      validateDiv.appendChild(validateResult);
+      validateDiv.appendChild(document.createElement('br'));
+    } */
+  }
+  const validateResult = document.createTextNode(`Congrats. All the rows and columns additions are iqual to 100`);
+  validateDiv.appendChild(validateResult);
 });
